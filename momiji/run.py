3cb8f32b63acb699a8038f7e544d08dbf3dc768e
taxfree_python <taxfreepython@gmail.com>
2024-06-17 19:31:58 +0900

    Add error message

--- momiji/run.py
+++ momiji/run.py
@@ -16,6 +16,15 @@ urlretrieve(url, file_path)
 
 model = torch.load(file_path)
 model.base_m
+"""
+Traceback (most recent call last):
+  File "/app/momiji/run.py", line 18, in <module>
+    model.base_m
+  File "/usr/local/lib/python3.10/site-packages/torch/nn/modules/module.py", line 1709, in __getattr__
+    raise AttributeError(f"'{type(self).__name__}' object has no attribute '{name}'")
+AttributeError: 'OcampoATAC1' object has no attribute 'base_m'
+"""
 
 predictor = Predictor(adata, model, batch_size=1024)
 preds = predictor(adata)
+
