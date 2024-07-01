5cf8ea8aeb7d6f0194943cd1fd07cb8edc2811bc
taxfree_python <taxfreepython@gmail.com>
2024-07-01 19:58:24 +0900

    chore: Update test_preprocess.py and add tests for df_to_adata function

--- tests/test_preprocess.py
+++ tests/test_preprocess.py
@@ -1,3 +1,5 @@
+import anndata
+import numpy as np
 import pandas as pd
 
 from momiji.preprocess._preprocess import df_to_adata
@@ -5,23 +7,31 @@ from momiji.preprocess._preprocess import df_to_adata
 
 def test_df_to_adata():
     # Create a dummy DataFrame
-    df = pd.DataFrame(
-        {
-            "gene1": [1, 2, 3, 4, 5],
-            "gene2": [6, 7, 8, 9, 10],
-            "gene3": [11, 12, 13, 14, 15],
-        },
-        index_names=[
-            "sample_1",
-            "sample_2",
-            "sample_3",
-            "sample_4",
-            "sample_5",
-        ],
-    )
+    df = pd.read_pickle("tests/GSE193140.pkl")
 
     # Convert the DataFrame to an anndata.AnnData object
     adata = df_to_adata(df)
 
+    # Assert that the adata object is of type anndata.AnnData
+    assert isinstance(adata, anndata.AnnData)
+
     # Assert that the adata object has the correct shape
-    assert adata.X.shape == (5, 3)
+    assert adata.X.shape == (157, 80400)
+
+    # Assert that the data in adata matches the original DataFrame
+    np.testing.assert_array_equal(adata.X, df.values)
+
+    # Assert that adata.obs and adata.var have correct dimensions
+    assert adata.obs.shape[0] == df.shape[0]
+    assert adata.var.shape[0] == df.shape[1]
+
+    # Check that the number of elements in adata.X is correct
+    assert adata.X.size == df.size
+
+    # Optionally, check some specific values in adata to ensure correctness
+    assert adata.X[0, 0] == df.iloc[0, 0]
+    assert adata.X[-1, -1] == df.iloc[-1, -1]
+
+    # Check that the column and row names are correctly set
+    assert list(adata.var_names) == list(df.columns)
+    assert list(adata.obs_names) == list(df.index)
